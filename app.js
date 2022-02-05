515d61c3d4523ba2dba2d5b2943f7df253cb7158
Knet-MAC <knet-macintosh@Kantathorns-MacBook-Air.local>
2022-02-05 13:55:20 +0700

    JS-DOM-Example

---
+++ app.js
@@ -0,0 +1,38 @@
+//document.body.append('Hello World','1','2') // เพิ่มสตริง
+
+// const h1 = document.createElement('h1')
+// const p = document.createElement('p')
+// // h1.innerText = 'Hello World'
+// h1.textContent = 'Hello Front End'
+
+// //const h1 = document.querySelectorAll('.hello')
+// // console.log(h1.innerText)
+// // console.log(h1.textContent)
+
+// h1.innerHTML = '<a href="#">Hello World</a>'
+// p.innerText = 'Hi'
+// // h1.innerText = '<a href="#">Hello World</a>'
+
+// document.body.appendChild(h1) // เพิ่ม tag node ลูก
+// document.body.appendChild(p)
+
+// p.remove()
+
+// document.body.appendChild(h1)
+// document.body.appendChild(p)
+
+// p.remove()
+
+const h1 = document.querySelector('#hello')
+// h1.id = 'A1'
+// h1.title = 'Hello Day 2
+
+// h1.dataset.textd = '4'
+
+// console.log(h1.classList.contains('hello'))
+// console.log(h1.classList.add('hello4'))
+// console.log(h1.classList.toggle('hello4'))
+// console.log(h1.classList.remove('hello4'))
+
+h1.style.backgroundColor = 'red'
+h1.style.color = 'white'
